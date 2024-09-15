ac450a647e79c4d2c7810eb21b6f0acea9f82ba7
Benjamin <benjaminalladi10@gmail.com>
2024-09-15 11:43:21 +0530

    added vercel

---
+++ backend/src/api/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import {connectDB} from "./db/index.js";
+
+import { app } from "./app.js";
+
+connectDB()
+.then((res)=>{
+    // app.listen(process.env.PORT || 8000, ()=>{
+    //     console.log(`server listening on port ${process.env.PORT}`);
+    // });
+
+    // app.on("error", (err)=>{
+    //     console.log("server error:", err);
+    // });
+
+    console.log("Database connected successfully.");
+})
+.catch((err)=> console.log("DB connection error:", err));
+
+export const handler = serverless(app);
